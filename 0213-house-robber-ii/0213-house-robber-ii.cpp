f3a2b5dea59292b63d9ba87b5f83cd553fd9d98a
Anjali Sharma <anjali34490@gmail.com>
2026-07-13 14:55:44 +0530

    Time: 0 ms (100%), Space: 10.9 MB (18.77%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    int fun(int index,vector<int>& nums, vector<int>& dp){
-        if(index<0) return 0;
-        if(index==0) return nums[0];
-        
-        if(dp[index]!=-1) return dp[index];
+   int sol(int i,vector<int>& nums,vector<int>& dp){
+    if(i<0) return 0;
+    if(i==0) return nums[0];
+    if(dp[i]!=-1) return dp[i];
+    int nottake=0+sol(i-1,nums,dp);
+    int take=nums[i]+sol(i-2,nums,dp);
 
-        int nottake=0+fun(index-1,nums,dp);
-        int take=nums[index]+fun(index-2,nums,dp);
-
-        return dp[index]=max(nottake,take);
-    }
+    return dp[i]=max(take,nottake);
+}
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n+1,-1);
-        if (n == 0) return 0;
+
         if (n == 1) return nums[0];
-        vector<int> first;
+
+        vector<int> fir;
         vector<int> sec;
 
         for(int i=0;i<n-1;i++){
-            first.push_back(nums[i]);
+            fir.push_back(nums[i]);
         }
         for(int i=1;i<n;i++){
             sec.push_back(nums[i]);
         }
-        
-        vector<int> dp1(first.size(), -1);
-        vector<int> dp2(sec.size(), -1);
 
-        int ans1=fun(first.size()-1,first,dp1);
-        int ans2=fun(sec.size()-1,sec,dp2);
+        vector<int> dp1(fir.size(),-1);
+        vector<int> dp2(sec.size(),-1);
+
+        int ans1=sol(fir.size()-1,fir,dp1);
+        int ans2=sol(sec.size()-1,sec,dp2);
 
         return max(ans1,ans2);
     }
