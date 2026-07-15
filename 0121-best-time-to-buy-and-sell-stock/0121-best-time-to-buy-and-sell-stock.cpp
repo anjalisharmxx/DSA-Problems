82cfbecd40c99834cc4277dab6a1df2d27119af0
Anjali Sharma <anjali34490@gmail.com>
2026-07-15 21:58:12 +0530

    Time: 0 ms (100%), Space: 97.2 MB (87.72%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int buy=prices[0];
         int sell=0;
 
-        for(int i=1;i<prices.size();i++){
+        for(int i=0;i<prices.size();i++){
              buy=min(buy,prices[i]);
              sell=max(sell,prices[i]-buy);
         }
